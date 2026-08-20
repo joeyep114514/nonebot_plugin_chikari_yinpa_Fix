7717eb73444cea4d89d9ecf074b5f7ec4055253b
joeyep114514 <joeyep114514@outlook.com>
2026-08-20 19:33:42 +0800

    v1.5.0.1:把字体换成了无衬线

--- nonebot_plugin_chikari_yinpa/config.py
+++ nonebot_plugin_chikari_yinpa/config.py
@@ -11,16 +11,11 @@ class Config(BaseModel):
     """初始深度。默认：10"""
     chikari_yinpa_initial_money: Optional[int] = 100
     """初始金钱。默认：100"""
-<<<<<<< HEAD
-    chikari_yinpa_font:Path = Path(__file__).parent / "resource" / "SourceHanSansSC-VF.ttf"
-    """绘图所用字体。默认：'模块路径/resource/SourceHanSansSC-VF.ttf'"""
-=======
     chikari_yinpa_transfer_unlock_money: Optional[int] = 10000
     """转账功能解锁所需金钱。默认：10000"""
     chikari_yinpa_transfer_cooldown: Optional[int] = 3600
     """转账冷却时间（秒）。默认：3600（1小时）"""
-    chikari_yinpa_font:Path = Path(__file__).parent / "resource" / "NotoSerifCJKsc-VF.ttf"
-    """绘图所用主字体。默认：'模块路径/resource/NotoSerifCJKsc-VF.ttf'"""
+    chikari_yinpa_font:Path = Path(__file__).parent / "resource" / "NotoSansCJKsc-VF.ttf"
+    """绘图所用主字体。默认：'模块路径/resource/NotoSansCJKsc-VF.ttf'"""
     chikari_yinpa_emoji_font:Path = Path(__file__).parent / "resource" / "NotoEmoji.ttf"
     """绘图所用 Emoji 字体。默认：'模块路径/resource/NotoEmoji.ttf'"""
->>>>>>> cc0ceec (v1.5.0 将字体换成了CJK字体，额外加了Emoji字体)
